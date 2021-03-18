4ba0e4afba958fefb354bf325fa6f4c5c26cbbf2
NoelCov <73734505+NoelCov@users.noreply.github.com>
2021-03-18 11:11:49 -0700

    Added some functionality

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,28 +8,40 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
 
   @override
-    void initState() {
-      super.initState();
-      getLocation();
-    }
-
-    @override
   void deactivate() {
     super.deactivate();
   }
 
-  void getLocation () async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    double longitude = location.longitude;
+    double latitude = location.latitude;
+
+    print('This is the longitude: $longitude');
+    print('This is the latitude: $latitude');
+  }
+
+  void getData() async {
+    http.Response response =
+        await http.get(Uri.https('https://API.com', '/path to what we want'));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold();
   }
-
 }
-
-
